f65fb8aaf088cb9577f08fe751058b60645946af
cfy <1002161225@qq.com>
2024-06-03 14:40:36 +0800

    2.0

--- src/iotroot.php
+++ src/iotroot.php
@@ -21,6 +21,9 @@ class iotroot {
         curl_setopt($this->curl, CURLOPT_HEADER, false);
         curl_setopt($this->curl, CURLOPT_FOLLOWLOCATION, 1);
         curl_setopt($this->curl, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($this->curl, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
+//        curl_setopt($this->curl, CURLOPT_PROXY, "127.0.0.1");
+//        curl_setopt($this->curl, CURLOPT_PROXYPORT, 8888);
     }
 
     /**
@@ -29,7 +32,7 @@ class iotroot {
      * @return array
      * @throws \Exception
      */
-    public function getTemplates() {
+    public function queryTemplateInfo() {
         curl_setopt($this->curl, CURLOPT_URL, $this->domain . "/company/queryTemplateInfo/" . $this->params);
 
         return $this->processRes(curl_exec($this->curl));
@@ -43,7 +46,7 @@ class iotroot {
      * @return array
      * @throws \Exception
      */
-    public function getProductStatus($returnId) {
+    public function queryProductStatus($returnId) {
         curl_setopt($this->curl, CURLOPT_URL, $this->domain . "/company/queryProductStatus/" . $this->params);
 
         return $this->processRes(curl_exec($this->curl));
@@ -52,18 +55,18 @@ class iotroot {
     /**
      * 分批次回传
      *
-     * @param string $code
+     * @param string $productCode
      * @param array $codeList
      * @param array $moduleList
      *
      * @return array
      * @throws \Exception
      */
-    public function returnProductBatch($code, $codeList, $moduleList) {
-        curl_setopt($this->curl, CURLOPT_URL, $this->domain . "/company/returnProductBatch/" . $this->params);
+    public function ecodeBatchReturn($productCode, $codeList, $moduleList) {
+        curl_setopt($this->curl, CURLOPT_URL, $this->domain . "/company/ecodeBatchReturn/" . $this->params);
         curl_setopt($this->curl, CURLOPT_POST, 1);
         curl_setopt($this->curl, CURLOPT_POSTFIELDS, json_encode([
-            'code'       => $code,
+            'code'       => $productCode,
             'codeList'   => $codeList,
             'moduleList' => $moduleList
         ]));
@@ -82,12 +85,12 @@ class iotroot {
      *
      * @return array
      */
-    public function returnProductInfo($name, $typeNumber, $templateId, $base64, $typeList) {
+    public function returnProduct($name, $typeNumber, $templateId, $base64, $typeList) {
         $data = [
             'productTypeNumber' => $typeNumber,
             'templeteId'        => $templateId,
             'productName'       => $name,
-            'photos'            => $base64,
+            'photos'            => is_array($base64) ? $base64 : [ $base64 ],
             'productTypelist'   => $typeList
         ];
 
@@ -95,6 +98,133 @@ class iotroot {
         curl_setopt($this->curl, CURLOPT_URL, $this->domain . "/company/returnProduct/" . $this->params);
         curl_setopt($this->curl, CURLOPT_POST, 1);
         curl_setopt($this->curl, CURLOPT_POSTFIELDS, json_encode($data));
+
+        return $this->processRes(curl_exec($this->curl));
+    }
+    
+    /**
+     * 编码申请
+     *
+     * @param int $num 申请数量，一次不能超过50000个
+     *
+     * @return array
+     * @throws \Exception
+     */
+    public function ecodeApply($num = 1) {
+        if ( $num > 50000 ) {
+            throw new \Exception('申请数量不能超过50000');
+        }
+        $params = 'clientId=' . encrypter::getClientId() . '/num=' . $num . '/timeStamp=' . encrypter::getTimeStamp() . '/sign=' . encrypter::getSignString();
+        curl_setopt($this->curl, CURLOPT_URL, $this->domain . "/company/ecodeApply/" . $params);
+
+        return $this->processRes(curl_exec($this->curl));
+    }
+
+    /**
+     * 从模板中过滤出必要的产品信息字段
+     * 
+     * @param $template
+     *
+     * @return array
+     */
+    public function productItemFilter($template) {
+        $typeList = [];
+        foreach ( $template as $result ) {
+            $num = 1;
+            for ( $i = 0; $i < 20; $i++) {
+                $index = 'tableList' . $num++;
+                if ( !isset($result[$index]) ) {
+                    break 1;
+                }
+                foreach ( $result[$index] as $item ) {
+                    $temp = $this->_productItemFilter($item);
+                    if ( !empty($temp['list']) ) {
+                        $typeList[] = $temp;
+                    }
+                }
+            }
+        }
+        return $typeList;
+    }
+
+    protected function _productItemFilter($tab) {
+        $list = [];
+        if ( $tab['isProduct'] === '1' ) {
+            foreach ( $tab['detailList'] as $item ) {
+                if ( $item['isProduct'] === '1' && $item['required'] === '1') {
+                    $list[] = [ 'name' => $item['name'] ];
+                }
+            }
+        }
+
+        return [
+            'type' => $tab['type'],
+            'list' => $list
+        ];
+    }
+    
+    /**
+     * 编码获取
+     * 
+     * @param string $fileId
+     *
+     * @return array
+     * @throws \Exception
+     */
+    public function queryEcodes($fileId) {
+        $codes = [];
+        $params = 'clientId=' . encrypter::getClientId() . '/fileId=' . $fileId . '/timeStamp=' . encrypter::getTimeStamp() . '/sign=' . encrypter::getSignString();
+        curl_setopt($this->curl, CURLOPT_URL, $this->domain . "/company/download/" . $params);
+        
+        // res is a zip stream
+        $res = curl_exec($this->curl);
+        
+        // check curl response status
+        if ( curl_getinfo($this->curl, CURLINFO_HTTP_CODE) !== 200 ) {
+            throw new \Exception('下载失败');
+        }
+        
+
+        // Save the zip stream to a temporary file
+        $tmpFile = tempnam(sys_get_temp_dir(), 'zip');
+        file_put_contents($tmpFile, $res);
+
+        // Open the zip file
+        $zip = new \ZipArchive;
+        if ( $zip->open($tmpFile) === true ) {
+            for ( $i = 0; $i < $zip->numFiles; $i ++ ) {
+//                $filename    = $zip->getNameIndex($i);
+                $fileContent = $zip->getFromIndex($i);
+                $fileContent = trim($fileContent);
+                $codes = array_merge($codes, explode("\n", $fileContent));
+            }
+            $zip->close();
+        } else {
+            throw new \Exception('读取下载内容失败');
+        }
+
+        // Delete the temporary file
+        unlink($tmpFile);
+        
+        return $codes;
+    }
+
+    /**
+     * 编码回退
+     * 
+     * @param array | string $ecodes
+     *
+     * @return array
+     * @throws \Exception
+     */
+    public function releaseEcodes($ecodes) {
+        if ( is_array($ecodes) ) {
+            $ecodes = implode(',', $ecodes);
+        }
+
+        curl_setopt($this->curl, CURLOPT_URL, $this->domain . "/company/fallbackByEcode/" . $this->params);
+        curl_setopt($this->curl, CURLOPT_POST, 1);
+        curl_setopt($this->curl, CURLOPT_POSTFIELDS, json_encode([ 'ecodes' => $ecodes ]));
 
         return $this->processRes(curl_exec($this->curl));
     }
